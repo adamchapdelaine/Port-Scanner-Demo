5a9aa73f80222d6654d1dba099069ef68fafcc9e
Adam Chapdelaine <chapdela@email.uscb.edu>
2026-03-29 14:09:02 -0400

    implement basic single-port TCP connection

--- scanner.py
+++ scanner.py
@@ -1,2 +1,20 @@
 import socket
-print(socket.gethostbyname('localhost'))
+
+# FIELDS
+address = '192.168.56.101'
+port = 22
+
+def scan(address, port):
+    # INET, streaming socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # try TCP connection
+    try:
+        s.connect((address, port))
+        print('Connected.')
+    
+    # exception if target port closed
+    except:
+        print('Not Connected.')
+    
+scan(address, port)    
