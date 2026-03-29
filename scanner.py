78dcdb3fabcdcaaf23a6f7dcc836ff429f46ace8
Adam Chapdelaine <chapdela@email.uscb.edu>
2026-03-29 14:41:56 -0400

    implement timeouts and proper socket closure

--- scanner.py
+++ scanner.py
@@ -2,19 +2,24 @@ import socket
 
 # FIELDS
 address = '192.168.56.101'
-port = 22
 
 def scan(address, port):
     # INET, streaming socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
     
     # try TCP connection
     try:
         s.connect((address, port))
-        print('Connected.')
+        print(f'[+] Port {port} is OPEN.')
     
     # exception if target port closed
     except:
-        print('Not Connected.')
-    
-scan(address, port)    
+        print(f'[-] Port {port} is CLOSED.')
+
+
+portRange = range(20, 31)
+
+# scans ports in range
+for p in portRange:
+    scan(address, p)    
